a53e1f96ab2157c71cbd965f75550035a58a243c
ikbalHasan-9703 <ikbalhasan37217@gmail.com>
2021-07-06 19:49:01 +0600

    valentina day project commiting

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # valentina-project
+# valentina-day-project
--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-$("")
+var loder = document.getElementById("preloader");
+window.addEventListener("load", function(){
+
+    loder.style.display = "none"
+})
